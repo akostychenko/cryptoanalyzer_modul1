22ce79c346210af0aaf34eec333f38159eafc1b8
”Aleksei <a.m.kostychenko@gmail.com>
2024-02-22 20:05:35 +0100

    added and edited directory Constants

--- src/constants/Alphabet.java
+++ src/constants/Alphabet.java
@@ -6,6 +6,6 @@ public class Alphabet {
             {'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п',
                     'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я',
                     '.', ',', '"', ':', '-', '!', '?', ' '};
-    private static final char[] SYMBOL_ALPHABET =
+    private static final char[] SYMBOLS_ALPHABET =
             {'.', ','};
 }
